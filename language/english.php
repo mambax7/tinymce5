9fb1a49c48b36214fe12208322907cc882d84930
Grégory Mage <gregory.mage@gmail.com>
2020-06-21 23:23:18 +0200

    initial commit

---
+++ language/english.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * TinyMCE5 adapter for XOOPS
+ *
+ * @category  XoopsEditor
+ * @package   TinyMCE5
+ * @author    Gregory Mage
+ * @author    Taiwen Jiang <phppp@users.sourceforge.net>
+ * @copyright 2020 XOOPS Project (http://xoops.org)
+ * @license   GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @link      http://xoops.org
+ */
+// Name of the editor
+define('_XOOPS_EDITOR_TINYMCE5', 'TinyMCE5');
+// The value must be the same as /tinymce/jscripts/langs/your_language_code, for example, "en" for English, "fr" for French
+// For details, check http://tinymce.moxiecode.com/download_i18n.php
+define('_XOOPS_EDITOR_TINYMCE_LANGUAGE5', 'en');
+// FONT LIST, FORMAT: "Name=value1,value2;Name=value"
+define('_XOOPS_EDITOR_TINYMCE_FONTS5', 'Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;Georgia=georgia,times new roman,times,serif;Tahoma=tahoma,arial,helvetica,sans-serif;Times New Roman=times new roman,times,serif;Verdana=verdana,arial,helvetica,sans-serif;Impact=impact;WingDings=wingdings');
